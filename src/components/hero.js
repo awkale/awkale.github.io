d81ec7ebd55be3b97e8060f2ad0fb6029cc308da
Alex Kale <hi@awkale.me>
2019-05-19 09:46:42 -0400

    move hero to styled component

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,15 +1,55 @@
-import React from 'react'
-import Img from 'gatsby-image'
+import React from 'react';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-import styles from './hero.module.css'
+const Hero = styled.div`
+  position: relative;
+  background: #000;
+  color: #fff;
+  text-align: center;
+`;
+
+const HeroImage = styled.div`
+  max-height: 400px;
+`;
+
+const HeroDetails = styled.div`
+  position: absolute;
+  background: rgba(0, 0, 0, 0.7);
+  left: 50%;
+  bottom: 0;
+  transform: translate(-50%, 0);
+  font-size: 14px;
+  padding: 0 0.5em;
+
+  @media (min-width: 600px) {
+    font-size: 16px;
+  }
+
+  @media (min-width: 1000px) {
+    font-size: 20px;
+  }
+`;
+
+const HeroHeadline = styled.h3`
+  margin: 0;
+`;
+
+const HeroTitle = styled.p`
+  margin: 0;
+  font-size: 1.125em;
+  font-weight: bold;
+`;
 
 export default ({ data }) => (
-  <div className={styles.hero}>
-    <Img className={styles.heroImage} alt={data.name} fluid={data.heroImage.fluid} />
-    <div className={styles.heroDetails}>
-      <h3 className={styles.heroHeadline}>{data.name}</h3>
-      <p className={styles.heroTitle}>{data.title}</p>
+  <Hero>
+    <HeroImage>
+      <Img alt={data.name} fluid={data.heroImage.fluid} />
+    </HeroImage>
+    <HeroDetails>
+      <HeroHeadline>{data.name}</HeroHeadline>
+      <HeroTitle>{data.title}</HeroTitle>
       <p>{data.shortBio.shortBio}</p>
-    </div>
-  </div>
-)
+    </HeroDetails>
+  </Hero>
+);
